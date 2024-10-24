6f7648b21b486a90a8ccbd92112f5e9ad9527de0
Fábio Mezzomo <fmezzomo@net11.com.br>
2024-10-24 18:14:29 -0300

    Add the new menu to WooCommerce menu

--- custom-user-api-integration.php
+++ custom-user-api-integration.php
@@ -14,3 +14,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Define plugin version and constants
 define( 'SAUCAL_FABIO_MEZZOMO_VERSION', '1.0' );
 define( 'SAUCAL_FABIO_MEZZOMO_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+// Register WooCommerce account tab and endpoint
+function saucal_fabio_mezzomo_add_account_tab( $items ) {
+    $new_items = array();
+    foreach ( $items as $key => $value ) {
+        // Add the new menu after Dashboard
+        $new_items[ $key ] = $value;
+
+        if ( $key === 'dashboard' ) {
+            $new_items['custom-data'] = __( 'Custom Data', 'saucal-fabio-mezzomo' );
+        }
+    }
+
+    return $new_items;
+}
+add_filter( 'woocommerce_account_menu_items', 'saucal_fabio_mezzomo_add_account_tab' );
