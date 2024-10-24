832780decdade340e0cd723542a814d3a937c98d
Fábio Mezzomo <fmezzomo@net11.com.br>
2024-10-24 18:51:23 -0300

    Register the endpoint

--- custom-user-api-integration.php
+++ custom-user-api-integration.php
@@ -23,10 +23,33 @@ function saucal_fabio_mezzomo_add_account_tab( $items ) {
         $new_items[ $key ] = $value;
 
         if ( $key === 'dashboard' ) {
-            $new_items['custom-data'] = __( 'Custom Data', 'saucal-fabio-mezzomo' );
+            $new_items['custom-data-api'] = __( 'Custom Data', 'saucal-fabio-mezzomo' );
         }
     }
 
     return $new_items;
 }
 add_filter( 'woocommerce_account_menu_items', 'saucal_fabio_mezzomo_add_account_tab' );
+
+function saucal_fabio_mezzomo_register_endpoint() {
+    add_rewrite_endpoint( 'custom-data-api', EP_ROOT | EP_PAGES );
+}
+add_action( 'init', 'saucal_fabio_mezzomo_register_endpoint' );
+
+function saucal_fabio_mezzomo_activate() {
+    saucal_fabio_mezzomo_register_endpoint();
+    flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'saucal_fabio_mezzomo_activate' );
+
+// To make sure that the rewrite rules will be clean when deactivate
+function saucal_fabio_mezzomo_deactivate() {
+    flush_rewrite_rules();
+}
+register_deactivation_hook( __FILE__, 'saucal_fabio_mezzomo_deactivate' );
+
+// Endpoint content
+function saucal_fabio_mezzomo_custom_data_content() {
+    echo '<h3>' . __( 'Custom Data', 'saucal-fabio-mezzomo' ) . '</h3>';
+}
+add_action( 'woocommerce_account_custom-data-api_endpoint', 'saucal_fabio_mezzomo_custom_data_content' );
