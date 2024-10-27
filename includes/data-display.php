a00951d1d87819891f541c67ce772558373d5963
Fábio Mezzomo <fmezzomo@net11.com.br>
2024-10-27 10:16:41 -0300

    Added nonce to the form

--- includes/data-display.php
+++ includes/data-display.php
@@ -7,13 +7,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Saucal_Fabio_Mezzomo_Data_Display {
 
     public function __construct() {
-        add_action( 'init', [ $this, 'handle_form_submission' ] );
+        add_shortcode( 'saucal_user_data', [ $this, 'display_user_data' ] );
     }
 
     public function display_user_data() {
         echo '<h3>' . __( 'Custom Data', 'saucal-fabio-mezzomo' ) . '</h3>';
         ?>
         <form method="post" action="">
+            <?php wp_nonce_field( 'saucal_fetch_elements', 'saucal_nonce' ); ?>
             <label for="element_list">
                 <?php echo __('Enter Elements (comma-separated):', 'saucal-fabio-mezzomo'); ?>
             </label>
@@ -21,12 +22,22 @@ class Saucal_Fabio_Mezzomo_Data_Display {
             <button type="submit">Fetch Elements</button>
         </form>
         <?php
-        if ( isset( $_POST['element_list'] ) ) {
-            $this->display_fetched_data();
+
+        if ( $this->is_form_submitted() ) {
+            $this->handle_form_submission();
         }
     }
 
-    private function display_fetched_data() {
+    private function is_form_submitted() {
+        return (
+            $_SERVER['REQUEST_METHOD'] === 'POST' &&
+            isset( $_POST['element_list'] ) &&
+            isset( $_POST['saucal_nonce'] ) &&
+            wp_verify_nonce( $_POST['saucal_nonce'], 'saucal_fetch_elements' )
+        );
+    }
+
+    private function handle_form_submission() {
         $element_list = sanitize_text_field( $_POST['element_list'] );
         $elements_array = explode( ',', $element_list );
 
@@ -37,12 +48,6 @@ class Saucal_Fabio_Mezzomo_Data_Display {
             echo '<pre>' . esc_html( print_r( $api_data, true ) ) . '</pre>';
         } else {
             echo '<p>' . __( 'No data available.', 'saucal-fabio-mezzomo' ) . '</p>';
-        }
-    }
-
-    public function handle_form_submission() {
-        if ( $_SERVER['REQUEST_METHOD'] === 'POST' && isset( $_POST['element_list'] ) ) {
-            $this->display_fetched_data();
         }
     }
 }
