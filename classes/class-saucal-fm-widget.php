fa2dfb0cae73df2a483563a4476b6422c094d198
Fábio Mezzomo <fmezzomo@net11.com.br>
2024-10-28 08:30:11 -0300

    Show the elements

--- classes/class-saucal-fm-widget.php
+++ classes/class-saucal-fm-widget.php
@@ -21,13 +21,21 @@ class Saucal_Fabio_Mezzomo_Widget extends WP_Widget {
         echo '<h3>' . __( 'Custom User Data', 'saucal-fabio-mezzomo' ) . '</h3>';
 
         $userID = get_current_user_id();
-        $preferences = get_user_meta( $userID, 'saucal_fm_user_preferences' );
+        $preferences = get_user_meta( $userID, 'saucal_fm_user_preferences', true );
         
         $apiHandler = new Saucal_Fabio_Mezzomo_API_Handler();
         $apiData = $apiHandler->fetch_elements( $preferences );
 
         if ( $apiData ) {
-            echo '<pre>' . esc_html( print_r( $apiData, true ) ) . '</pre>';
+            
+            if ( isset( $apiData['json']['elements'] ) && is_array( $apiData['json']['elements'] ) ) {
+                foreach ( $apiData['json']['elements'] as $element ) {
+                    echo '<p>' . esc_html( $element ) . '</p>';
+                }
+            } else {
+                echo '<p>No elements found.</p>';
+            }
+
         } else {
             echo '<p>' . __( 'No data available.', 'saucal-fabio-mezzomo' ) . '</p>';
         }
